aed64423c95b277f773e3a174943cd779f996421
yanjixing <78809118+yanjixing@users.noreply.github.com>
2021-02-18 22:37:49 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,4 +14,4 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#sed -i '$a src-git liuran001_packages https://github.com/liuran001/openwrt-packages' feeds.conf.default
+sed -i '$a src-git liuran001_packages https://github.com/yanjixing/openwrt-packages/tree/packages-19.07' feeds.conf.default
