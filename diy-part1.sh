9d84acb954e0d5633b5299b9958b467d97ba6a64
yanjixing <78809118+yanjixing@users.noreply.github.com>
2021-02-19 14:45:38 -1000

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,4 +16,4 @@
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 #git clone https://github.com/awesome-openwrt/luci-app-usb-printer.git package/luci-app-usb-printer
-svn co https://github.com.cnpmjs.org/yanjixing/lede
+#svn co https://github.com.cnpmjs.org/yanjixing/lede
