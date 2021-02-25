0ae02589f25dace1d1e52ead66dceffacea23ecb
yanjixing <78809118+yanjixing@users.noreply.github.com>
2021-02-24 20:16:31 -1000

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,6 +20,8 @@
 
 svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
 
+chmod 777 package/luci-app-openclash
+
 git clone https://github.com/awesome-openwrt/luci-app-usb-printer.git package/luci-app-usb-printer
 
 #git clone https://github.com/frainzy1477/luci-app-clash.git package/luci-app-clash
