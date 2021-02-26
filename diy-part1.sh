b903d01b73876705cf61a1f1e6b5c7405df818d2
yanjixing <78809118+yanjixing@users.noreply.github.com>
2021-02-25 16:01:25 -1000

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -25,19 +25,3 @@ svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/l
 git clone https://github.com/awesome-openwrt/luci-app-usb-printer.git package/luci-app-usb-printer
 
 #git clone https://github.com/frainzy1477/luci-app-clash.git package/luci-app-clash
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
-cd package/base-files/files
-mkdir -p etc/openclash/core
-          # for updates, go to: https://github.com/Dreamacro/clash/releases
-wget -qO- https://github.com/Dreamacro/clash/releases/download/v1.3.5/clash-linux-armv8-v1.3.5.gz | gunzip -c > etc/openclash/core/clash
-          # for updates, go to: https://github.com/vernesong/OpenClash/releases/tag/TUN-Premium
-wget -qO- https://github.com/vernesong/OpenClash/releases/download/TUN-Premium/clash-linux-armv8-2021.01.01.g0ab75c5.gz | gunzip -c > etc/openclash/core/clash_tun
-          # for updates, go to: https://github.com/vernesong/OpenClash/releases/tag/TUN
-wget -qO- https://github.com/vernesong/OpenClash/releases/download/TUN/clash-linux-armv8.tar.gz | tar xOvz > etc/openclash/core/clash_game
-chmod +x etc/openclash/core/clash*
-
-cd openwrt
-
-rm -rf feeds/packages/libs/libcap
-svn co https://github.com/openwrt/packages/trunk/libs/libcap feeds/packages/libs/libcap
-          
