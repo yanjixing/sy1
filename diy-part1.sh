60c112bcef3bf6dece55eeb464bebfa49abf553b
yanjixing <78809118+yanjixing@users.noreply.github.com>
2021-07-01 21:54:21 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -23,10 +23,10 @@
 #chmod 777 package/luci-app-openclash
 #git clone https://github.com/yanjixing/luci-app-usb-printer.git package/luci-app-usb-printer
 #unzip -j lean.zip -d package/
-git clone https://github.com/awesome-openwrt/luci-app-usb-printer.git package/luci-app-usb-printer
+#git clone https://github.com/awesome-openwrt/luci-app-usb-printer.git package/luci-app-usb-printer
 
 #git clone -b legacy https://github.com/kuoruan/luci-app-v2ray package/luci-app-v2ray
-git clone https://github.com/yanjixing/lean package/lede
+#git clone https://github.com/yanjixing/lean package/lede
 #git clone https://github.com/Apocalypsor/luci-app-smartdns.git package/luci-app-smartdns
 #git clone https://github.com/frainzy1477/luci-app-clash.git package/luci-app-clash
 
